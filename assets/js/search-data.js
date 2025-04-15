3a52e7bf33c6a3077ead1cf42577e9d292c7c3fa
Lee-daeho <54104389+Lee-daeho@users.noreply.github.com>
2025-04-15 09:27:51 +0000

    Deploying to gh-pages from @ Lee-daeho/Lee-daeho.github.io@ceb6fa309e426e76ecb73a1858fd192f8ab88a30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -119,8 +119,8 @@ ninja.data = [{
           description: "Worked as Team member &amp; Sub Software Developer, MicroController (Atmega 128), C#, DC Motor (2018. 02 ~ 2018. 07)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-harvesting-robot",
-          title: 'Harvesting Robot',
+            },},{id: "projects-please-note-that-if-you-don-39-t-wear-a-face-mask-you-restrict-to",
+          title: 'Please note that if you don&amp;#39;t wear a face mask, you restrict to...',
           description: "Wokred as Team Member &amp; Main Software Developer, Python, Pytorch, Real-Time, Generative AI. 2021. 03 ~ 2021. 04",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
