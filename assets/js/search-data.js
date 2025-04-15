2e619c39e538ee4dbaebb408367bfeda1de348f5
Lee-daeho <54104389+Lee-daeho@users.noreply.github.com>
2025-04-15 08:23:34 +0000

    Deploying to gh-pages from @ Lee-daeho/Lee-daeho.github.io@6617d8fb0000b5a824166d0061d09e82035f82c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -119,6 +119,11 @@ ninja.data = [{
           description: "Worked as Team member &amp; Sub Software Developer, MicroController (Atmega 128), C#, DC Motor (2018. 02 ~ 2018. 07)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-harvesting-robot",
+          title: 'Harvesting Robot',
+          description: "Wokred as Team Member &amp; Main Software Developer, Python, Pytorch, Real-Time, Generative AI. 2021. 03 ~ 2021. 04",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
